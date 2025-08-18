036e6edfaeab0f3065f7d255ad568ba337c8c87d
madacorn <codingstain@gmail.com>
2025-08-18 15:04:22 +0200

    Finishing component_pool + tests

--- src/component_pool.hpp
+++ src/component_pool.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <stdexcept>
+#include <utility>
 #include <vector>
 
 #include "acorn_assert.hpp"
@@ -11,7 +13,15 @@ template <class T>
 class ComponentPool
 {
 public:
-    explicit ComponentPool(const EntityManager& em) noexcept : em_(em) {}
+    explicit ComponentPool(const EntityManager& em, size_t reserve_hint = 0) noexcept : em_(em)
+    {
+        if (reserve_hint)
+        {
+            dense_entities_.reserve(reserve_hint);
+            dense_data_.reserve(reserve_hint);
+            sparse_.reserve(reserve_hint);
+        }
+    }
 
     bool has(Entity e) const noexcept
     {
@@ -27,7 +37,7 @@ public:
         return dense_entities_[pos] == e;
     }
 
-    T* try_get(Entity e)
+    T* try_get(Entity e) noexcept
     {
         if (!has(e))
             return nullptr;
@@ -35,11 +45,25 @@ public:
         return &dense_data_[sparse_[e.index]];
     }
 
-    T get(Entity e)
+    const T* try_get(Entity e) const noexcept
     {
-        auto* p = try_get(e);
-        ACORN_ASSERT(p);
-        return *p;
+        if (!has(e))
+            return nullptr;
+        return &dense_data_[sparse_[e.index]];
+    }
+
+    [[nodiscard]] T& get(Entity e)
+    {
+        if (auto* p = try_get(e))
+            return *p;
+        throw std::logic_error("get<T> on entity without component");
+    }
+
+    [[nodiscard]] const T& get(Entity e) const
+    {
+        if (auto* p = try_get(e))
+            return *p;
+        throw std::logic_error("get<T> on entity without component");
     }
 
     template <class... Args>
@@ -49,6 +73,7 @@ public:
 
         grow_sparse_to_fit(e.index);
 
+        // Overwrite policy, if the entity already has the component we overwrite it
         if (has(e))
         {
             auto pos = sparse_[e.index];
@@ -64,7 +89,7 @@ public:
             const auto pos = static_cast<uint32_t>(dense_data_.size());
 
             dense_entities_.push_back(e);
-            dense_data_.emplace_back(T(std::forward<Args>(args)...));
+            dense_data_.emplace_back(std::forward<Args>(args)...);
             sparse_[e.index] = pos;
 
 #ifndef NDEBUG
@@ -72,6 +97,78 @@ public:
 #endif
             return dense_data_.back();
         }
+    }
+
+    bool remove(Entity e) noexcept
+    {
+        if (!has(e))
+            return false;
+        auto pos = sparse_[e.index];
+        auto last = static_cast<uint32_t>(dense_data_.size() - 1);
+
+        if (pos != last)
+        {
+            const Entity moved = dense_entities_[last];
+
+            dense_data_[pos] = std::move(dense_data_[last]);
+            dense_entities_[pos] = moved;
+            sparse_[moved.index] = pos;
+        }
+
+        sparse_[e.index] = kAbsent;
+
+        dense_data_.pop_back();
+        dense_entities_.pop_back();
+
+#ifndef NDEBUG
+        debug_check_invariants();
+#endif
+        return true;
+    }
+
+    size_t size() const noexcept
+    {
+        return dense_data_.size();
+    }
+
+    bool empty() const noexcept
+    {
+        return dense_data_.empty();
+    }
+
+    size_t capacity() const noexcept
+    {
+        return dense_data_.capacity();
+    }
+
+    auto begin() noexcept
+    {
+        return dense_data_.begin();
+    }
+
+    auto end() noexcept
+    {
+        return dense_data_.end();
+    }
+
+    auto begin() const noexcept
+    {
+        return dense_data_.begin();
+    }
+
+    auto end() const noexcept
+    {
+        return dense_data_.end();
+    }
+
+    auto cbegin() const noexcept
+    {
+        return dense_data_.cbegin();
+    }
+
+    auto cend() const noexcept
+    {
+        return dense_data_.cend();
     }
 
 private:
--- tests/component_pool_test.cpp
+++ tests/component_pool_test.cpp
@@ -2,6 +2,11 @@
 
 #include <gtest/gtest.h>
 
+#include <stdexcept>
+#include <vector>
+
+#include "entity_manager.hpp"
+
 TEST(ComponentPoolTest, HasOnEmptyIsFalse)
 {
     acorn::EntityManager em;
@@ -11,7 +16,7 @@ TEST(ComponentPoolTest, HasOnEmptyIsFalse)
     EXPECT_FALSE(pool.has(e));
 }
 
-TEST(ComponentPool, TryGetReturnsNullWhenAbsent)
+TEST(ComponentPoolTest, TryGetReturnsNullWhenAbsent)
 {
     acorn::EntityManager em;
     acorn::ComponentPool<int> pool(em);
@@ -19,7 +24,7 @@ TEST(ComponentPool, TryGetReturnsNullWhenAbsent)
     EXPECT_EQ(pool.try_get(e), nullptr);
 }
 
-TEST(ComponentPool, EmplaceThenHasAndTryGet)
+TEST(ComponentPoolTest, EmplaceThenHasAndTryGet)
 {
     acorn::EntityManager em;
     acorn::ComponentPool<int> pool(em);
@@ -32,7 +37,7 @@ TEST(ComponentPool, EmplaceThenHasAndTryGet)
     EXPECT_EQ(*p, 42);
 }
 
-TEST(ComponentPool, DuplicateEmplaceOverwrites)
+TEST(ComponentPoolTest, DuplicateEmplaceOverwrites)
 {
     acorn::EntityManager em;
     acorn::ComponentPool<int> pool(em);
@@ -43,7 +48,7 @@ TEST(ComponentPool, DuplicateEmplaceOverwrites)
     EXPECT_EQ(*pool.try_get(e), 7);
 }
 
-TEST(ComponentPool, EmplaceLargeIndexGrowsSparse)
+TEST(ComponentPoolTest, EmplaceLargeIndexGrowsSparse)
 {
     acorn::EntityManager em;
     acorn::ComponentPool<int> pool(em);
@@ -53,4 +58,153 @@ TEST(ComponentPool, EmplaceLargeIndexGrowsSparse)
     auto e = v.back();
     pool.emplace(e, 9);
     EXPECT_TRUE(pool.has(e));
+}
+
+TEST(ComponentPoolTest, RemoveExistingClearsHasAndShrinks)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+
+    auto e = em.create();
+    pool.emplace(e, 5);
+    ASSERT_TRUE(pool.has(e));
+    const auto before = pool.size();
+
+    EXPECT_TRUE(pool.remove(e));
+    EXPECT_FALSE(pool.has(e));
+    EXPECT_EQ(pool.size(), before - 1);
+}
+
+TEST(ComponentPoolTest, RemoveMiddleSwapRemoveUpdatesSparse)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+
+    auto e0 = em.create();
+    auto e1 = em.create();
+    auto e2 = em.create();
+
+    pool.emplace(e0, 10);
+    pool.emplace(e1, 20);
+    pool.emplace(e2, 30);
+
+    EXPECT_TRUE(pool.remove(e1));
+    EXPECT_FALSE(pool.has(e1));
+    EXPECT_TRUE(pool.has(e0));
+    EXPECT_TRUE(pool.has(e2));
+
+    EXPECT_EQ(*pool.try_get(e0), 10);
+    EXPECT_EQ(*pool.try_get(e2), 30);
+}
+
+TEST(ComponentPoolTest, RemoveLastFastPath)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+
+    auto a = em.create();
+    auto b = em.create();
+
+    pool.emplace(a, 1);
+    pool.emplace(b, 2);  // b is last
+
+    EXPECT_TRUE(pool.remove(b));  // no swap
+    EXPECT_TRUE(pool.has(a));
+    EXPECT_FALSE(pool.has(b));
+    EXPECT_EQ(*pool.try_get(a), 1);
+}
+
+TEST(ComponentPoolTest, RemoveAbsentReturnsFalse)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+
+    auto e = em.create();
+    EXPECT_FALSE(pool.remove(e));  // never added
+    pool.emplace(e, 1);
+    EXPECT_TRUE(pool.remove(e));
+    EXPECT_FALSE(pool.remove(e));  // second time = false
+}
+
+TEST(ComponentPoolTest, IterateVisitsAll)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+
+    auto a = em.create();
+    auto b = em.create();
+    pool.emplace(a, 3);
+    pool.emplace(b, 4);
+
+    size_t count = 0;
+    int sum = 0;
+    for (int& v : pool)
+    {
+        ++count;
+        sum += v;
+    }
+    EXPECT_EQ(count, pool.size());
+    EXPECT_EQ(sum, 7);
+}
+
+TEST(ComponentPoolTest, HasRespectsGenerationAfterDestroy)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+
+    auto e = em.create();
+    pool.emplace(e, 5);
+    ASSERT_TRUE(pool.has(e));
+
+    EXPECT_TRUE(em.destroy(e));
+    EXPECT_FALSE(pool.has(e));
+    EXPECT_EQ(pool.try_get(e), nullptr);
+}
+
+TEST(ComponentPoolTest, GetReturnsReference)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+    auto e = em.create();
+    pool.emplace(e, 5);
+    int& ref = pool.get(e);
+    ref = 9;
+    EXPECT_EQ(*pool.try_get(e), 9);  // proves aliasing, not a copy
+}
+
+TEST(ComponentPoolTest, ConstTryGetAndGet)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+    auto e = em.create();
+    pool.emplace(e, 11);
+
+    const auto& cpool = pool;
+    const int* p = cpool.try_get(e);
+    ASSERT_NE(p, nullptr);
+    EXPECT_EQ(*p, 11);
+    EXPECT_EQ(cpool.get(e), 11);
+}
+
+TEST(ComponentPoolTest, GetAbsentThrows)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+    auto e = em.create();
+    EXPECT_THROW((void)pool.get(e), std::logic_error);
+}
+
+TEST(ComponentPoolTest, ReAddAfterRemoveWorks)
+{
+    acorn::EntityManager em;
+    acorn::ComponentPool<int> pool(em);
+
+    auto e = em.create();
+    pool.emplace(e, 5);
+    EXPECT_TRUE(pool.remove(e));
+    EXPECT_FALSE(pool.has(e));
+
+    pool.emplace(e, 7);
+    EXPECT_TRUE(pool.has(e));
+    EXPECT_EQ(*pool.try_get(e), 7);
 }
