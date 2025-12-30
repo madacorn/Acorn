da3682c13f192d45456dcaad7afa38488730d861
github-action-benchmark <github@users.noreply.github.com>
2025-12-30 16:58:02 +0000

    add Benchmark (googlecpp) benchmark result for ce65c468008383c604f41b71e131c04d3183b226

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767113119940,
+  "lastUpdate": 1767113882073,
   "repoUrl": "https://github.com/madacorn/Acorn",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 262682.28362133633,
             "unit": "ns/iter",
             "extra": "iterations: 2662\ncpu: 262659.2824943652 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "codingstain@gmail.com",
+            "name": "madacorn",
+            "username": "madacorn"
+          },
+          "committer": {
+            "email": "codingstain@gmail.com",
+            "name": "madacorn",
+            "username": "madacorn"
+          },
+          "distinct": true,
+          "id": "ce65c468008383c604f41b71e131c04d3183b226",
+          "message": "Update readme with proper information and structure.",
+          "timestamp": "2025-12-30T17:57:10+01:00",
+          "tree_id": "7ea0608efb6429318cbd9c8e87d72652fd5742fd",
+          "url": "https://github.com/madacorn/Acorn/commit/ce65c468008383c604f41b71e131c04d3183b226"
+        },
+        "date": 1767113881804,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_EntityCreation",
+            "value": 2.3399531167507903,
+            "unit": "ns/iter",
+            "extra": "iterations: 327991345\ncpu: 2.3396831279191224 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ComponentAdd",
+            "value": 15.660018279129613,
+            "unit": "ns/iter",
+            "extra": "iterations: 44669528\ncpu: 15.659221449575202 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ViewIteration/1000",
+            "value": 26204.90311574063,
+            "unit": "ns/iter",
+            "extra": "iterations: 26671\ncpu: 26202.31532375989 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ViewIteration/4096",
+            "value": 107146.15994479603,
+            "unit": "ns/iter",
+            "extra": "iterations: 6521\ncpu: 107129.94678730259 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_ViewIteration/10000",
+            "value": 262087.12735492006,
+            "unit": "ns/iter",
+            "extra": "iterations: 2654\ncpu: 262046.17897513183 ns\nthreads: 1"
           }
         ]
       }
